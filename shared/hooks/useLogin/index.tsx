fbd0119b1296222a0a02836de75d7ae00c8e2fee
ranoob <whyfck@gmail.com>
2023-02-02 13:05:44 -0300

    fix: added try catch

--- shared/hooks/useLogin/index.tsx
+++ shared/hooks/useLogin/index.tsx
@@ -2,18 +2,22 @@ import { UserData } from '@/shared/templates/LoginTemplate'
 import { apiURL } from '@/shared/lib/api'
 
 async function loginFetch(username: string, password: string) {
-  const data = await fetch(apiURL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      username,
-      password
+  try {
+    const data = await fetch(apiURL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username,
+        password
+      })
     })
-  })
-    .then((res) => res.json())
-    .then((data) => data)
+      .then((res) => res.json())
+      .then((data) => data)
 
-  return data as UserData
+    return data as UserData
+  } catch (err: any) {
+    throw Error(err)
+  }
 }
 
 export function useLogin() {
